a3363828370a9a4858d87e2c67049a19cc17363f
Zuiron <martinfloden90@gmail.com>
2024-12-11 19:52:28 +0100

    add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import LatestIssues from "./LatestIssues";
 import Pagination from "./components/Pagination";
 import IssueChart from "./IssueChart";
 import { Flex, Grid } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -25,3 +26,8 @@ export default async function Home() {
     </Grid>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of project issues",
+};
